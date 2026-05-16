20102e8124de338b06f35ceae956c9ecb71e7205
danidev-100 <adriandanielfernandez@gmail.com>
2026-05-16 11:05:49 -0300

    prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "schema.prisma",
+  schema: "backend/prisma/schema.prisma",
   datasource: {
     url: process.env.DATABASE_URL!,
   },
