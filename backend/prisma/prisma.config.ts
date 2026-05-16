6cb8dce91d8d45a1e1e34373fa7b744f168943c4
danidev-100 <adriandanielfernandez@gmail.com>
2026-05-16 11:18:24 -0300

    db produccion

--- backend/prisma/prisma.config.ts
+++ backend/prisma/prisma.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "schema.prisma",
+  migrations: {
+    path: "migrations",
+  },
   datasource: {
     url: process.env.DATABASE_URL!,
   },
